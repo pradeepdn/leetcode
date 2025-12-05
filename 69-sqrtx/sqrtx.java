0bdb2fb2b212ce1d3f42f93e75aea57f1d35f187
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-05 11:59:36 -0800

    Time: 1 ms (92.38%) | Memory: 42.7 MB (7.47%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,12 +1,32 @@
 class Solution {
     public int mySqrt(int x) {
+        /* Newton–Raphson */
+
+        // if (x == 0) return 0;
+        // double guess = x / 2.0;
+
+        // while (Math.abs(guess*guess - x) > 1e-6){
+        //     guess = (guess + x / guess) / 2.0;
+        // }
+
+        // return (int)guess; 
+
+        /* Binary Search */
+
+        int left = 1;
+        int right = x;
+        int res = 0;
         if (x == 0) return 0;
-        double guess = x / 2.0;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) { 
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
 
-        while (Math.abs(guess*guess - x) > 1e-6){
-            guess = (guess + x / guess) / 2.0;
-        }
-
-        return (int)guess;
+        }   
+    return res;
     }
 }
