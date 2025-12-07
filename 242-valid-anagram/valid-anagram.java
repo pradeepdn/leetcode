e83fc4cd44e6a1ecf94cceabbc332f7498ea5d24
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-06 23:34:17 -0800

    Time: 4 ms (78.79%) | Memory: 44.2 MB (74.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,47 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if(s.length() != t.length()) return false;
-        Map<Character, Integer> mag = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            mag.put(c, mag.getOrDefault(c, 0) + 1);
+            count[c - 'a']++;
         }
 
         for (int i = 0; i < t.length(); i++) {
             char c = t.charAt(i);
-            if (!mag.containsKey(c) || mag.get(c) == 0) {
+            if (count[c - 'a'] == 0) {
                 return false;
             }
-            mag.put(c, mag.get(c) - 1);
+            count[c - 'a']--;
         }
         return true;
-
     }
 }
+/* Using HashMap */
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+//         Map<Character, Integer> mag = new HashMap<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             mag.put(c, mag.getOrDefault(c, 0) + 1);
+//         }
+
+//         for (int i = 0; i < t.length(); i++) {
+//             char c = t.charAt(i);
+//             if (!mag.containsKey(c) || mag.get(c) == 0) {
+//                 return false;
+//             }
+//             mag.put(c, mag.get(c) - 1);
+//         }
+//         return true;
+
+//     }
+// }
