fa0c1954299c9283ac94fe6bce7ea2226e412a3a
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-06 23:37:28 -0800

    Time: 1 ms (99.71%) | Memory: 46.3 MB (37.15%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,41 +1,41 @@
-/* Using HashMap */
+/* Using Arrays */
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> mag = new HashMap<>();
+        int[] count = new int[26];
 
         for (int i = 0; i < magazine.length(); i++) {
             char c = magazine.charAt(i);
-            mag.put(c, mag.getOrDefault(c, 0) + 1);
+            count[c - 'a']++;
         }
 
         for (int i = 0; i < ransomNote.length(); i++) {
             char c = ransomNote.charAt(i);
-            if (!mag.containsKey(c) || mag.get(c) == 0) {
+            if (count[c - 'a'] == 0) {
                 return false;
             }
-            mag.put(c, mag.get(c) - 1);
+            count[c - 'a']--;
         }
         return true;
-
     }
 }
-/* Using Arrays */
+/* Using HashMap */
 // class Solution {
 //     public boolean canConstruct(String ransomNote, String magazine) {
-//         int[] count = new int[26];
+//         Map<Character, Integer> mag = new HashMap<>();
 
 //         for (int i = 0; i < magazine.length(); i++) {
 //             char c = magazine.charAt(i);
-//             count[c - 'a']++;
+//             mag.put(c, mag.getOrDefault(c, 0) + 1);
 //         }
 
 //         for (int i = 0; i < ransomNote.length(); i++) {
 //             char c = ransomNote.charAt(i);
-//             if (count[c - 'a'] == 0) {
+//             if (!mag.containsKey(c) || mag.get(c) == 0) {
 //                 return false;
 //             }
-//             count[c - 'a']--;
+//             mag.put(c, mag.get(c) - 1);
 //         }
 //         return true;
+
 //     }
 // }
