53c280e0c84900b86cf3c63976cc97fbae416294
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-06 21:14:28 -0800

    Time: 1 ms (99.71%) | Memory: 46.5 MB (32.04%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (count[c - 'a'] == 0) {
+                return false;
+            }
+            count[c - 'a']--;
+        }
+        return true;
+    }
+}
