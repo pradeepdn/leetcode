fbc95a78e7c99314be80120c7726c088eee711b6
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-08 10:23:49 -0800

    Time: 2 ms (17.90%) | Memory: 42.2 MB (44.62%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,22 +1,17 @@
+/** Improved version */
 class Solution {
-
     public boolean isHappy(int n) {
-        if(n==1) return true;
         HashSet<Integer> squares = new HashSet<>();
-        squares.add(n);
-        int num = squaresOfDigits(n);
 
-        while (squares.contains(num) == false) {
-            if (num == 1) {
-                return true;
+        while (n!=1){
+            n = squaresOfDigits(n);
+
+            if(!squares.add(n)){
+                return false;
             }
-            squares.add(num);
-            num = squaresOfDigits(num);
         }
-        return false;
-
+        return true;
     }
-
     public int squaresOfDigits(int k) {
         int square = 0;
         while (k != 0) {
@@ -26,3 +21,34 @@ class Solution {
         return square;
     }
 }
+
+
+/** My first solution  */
+// class Solution {
+
+//     public boolean isHappy(int n) {
+//         if(n==1) return true;
+//         HashSet<Integer> squares = new HashSet<>();
+//         squares.add(n);
+//         int num = squaresOfDigits(n);
+
+//         while (squares.contains(num) == false) {
+//             if (num == 1) {
+//                 return true;
+//             }
+//             squares.add(num);
+//             num = squaresOfDigits(num);
+//         }
+//         return false;
+
+//     }
+
+//     public int squaresOfDigits(int k) {
+//         int square = 0;
+//         while (k != 0) {
+//             square = square + ((k % 10) * (k % 10));
+//             k = k / 10;
+//         }
+//         return square;
+//     }
+// }
