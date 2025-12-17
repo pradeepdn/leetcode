545fbb444a8769a72f7f5c225e5b4069478afc52
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-16 16:19:10 -0800

    Time: 2 ms (91.03%) | Memory: 42.8 MB (61.55%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        
 
         ArrayList<String> summary = new ArrayList<>();
         int start = 0;
@@ -15,16 +14,14 @@ class Solution {
 
             if (nums[i + 1] != nums[i] + 1) {
                 if (start == nums[i]) {
-                    System.out.println(nums.length);
                     summary.add(String.valueOf(start));
                     start = nums[i + 1];
-                }else{
-                summary.add(start + "->" + nums[i]);
-                start = nums[i + 1];
+                } else {
+                    summary.add(start + "->" + nums[i]);
+                    start = nums[i + 1];
                 }
             }
             if (i == nums.length - 2) {
-                //System.out.println(nums.length);
                 if (start != nums[i + 1]) {
                     summary.add(start + "->" + nums[i + 1]);
                 } else if (start == nums[i + 1]) {
