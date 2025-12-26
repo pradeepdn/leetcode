5a3d3f799aa14a39177479aa6bf50a77eab2b94c
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-25 18:11:18 -0700

    Time: 0 ms (100.00%) | Memory: 44.3 MB (8.61%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,44 +15,44 @@
  */
 class Solution {
 
-    Queue<TreeNode> preOrder = new LinkedList<>();
-
-    public void flatten(TreeNode root) {
-        preOrder(root);
-
-        TreeNode curr = preOrder.poll();
-
-        while(!preOrder.isEmpty()){
-            TreeNode nextNode = preOrder.poll();
-            curr.left = null;
-            curr.right = nextNode;
-            curr = nextNode;
-        }
-    }
-
-    private void preOrder(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        preOrder.add(node);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
+    // Queue<TreeNode> preOrder = new LinkedList<>();
 
     // public void flatten(TreeNode root) {
-    //     TreeNode curr = root;
-    //     while (curr != null) {
-    //         if (curr.left != null) {
-    //             TreeNode prev = curr.left;
-    //             while (prev.right != null) {
-    //                 prev = prev.right;
-    //             }
-    //             prev.right = curr.right;
-    //             curr.right = curr.left;
-    //             curr.left = null;
+    //     preOrder(root);
 
-    //         }
-    //         curr = curr.right;
+    //     TreeNode curr = preOrder.poll();
+
+    //     while(!preOrder.isEmpty()){
+    //         TreeNode nextNode = preOrder.poll();
+    //         curr.left = null;
+    //         curr.right = nextNode;
+    //         curr = nextNode;
     //     }
     // }
+
+    // private void preOrder(TreeNode node) {
+    //     if (node == null) {
+    //         return;
+    //     }
+    //     preOrder.add(node);
+    //     preOrder(node.left);
+    //     preOrder(node.right);
+    // }
+
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode prev = curr.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+
+            }
+            curr = curr.right;
+        }
+    }
 }
